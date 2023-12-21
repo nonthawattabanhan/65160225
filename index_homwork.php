197c57bb5f99d4301f545825661e923cf277446a
nonthawattabanhan <65160225@go.buu.ac.th>
2023-12-21 18:31:55 +0700

    php

---
+++ index_homwork.php
@@ -0,0 +1,23 @@
+<html>
+    <body>
+    <h1>this is PHP <?='Hello World'; ?></h1>
+    <div>
+        <p>1</p>
+        <?php
+        $var_a = 1;
+        $var_a ="1";
+        print_r($var_a);
+        var_dump($var_a);
+        echo $var_a + 2;
+        {
+            echo "<h1>my h1</h1>";
+            ?>
+            <p>2</p>
+            <?php
+        }
+        ?>
+        <p>3</p>
+        <p>4</p>
+    </div>
+    </body>
+</html>
